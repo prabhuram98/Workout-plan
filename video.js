69e05f86db9a4226584a7e589c23478538b610f2
prabhuram98 <prabhuram9841@gmail.com>
2026-06-28 09:10:15 +0100

    Create video.js

---
+++ video.js
@@ -0,0 +1,19 @@
+const VIDEOS = {
+
+"Leg press": "https://www.youtube.com/results?search_query=leg+press+proper+form",
+"Chest press (machine or dumbbell)": "https://www.youtube.com/results?search_query=chest+press+proper+form",
+"Lat pulldown": "https://www.youtube.com/results?search_query=lat+pulldown+proper+form",
+"Shoulder press": "https://www.youtube.com/results?search_query=shoulder+press+proper+form",
+"Plank": "https://www.youtube.com/results?search_query=plank+exercise+form",
+"Cable crunch": "https://www.youtube.com/results?search_query=cable+crunch+abs+exercise",
+"Incline walk": "https://www.youtube.com/results?search_query=incline+walking+treadmill",
+"Squats": "https://www.youtube.com/results?search_query=squat+proper+form",
+"Push-ups": "https://www.youtube.com/results?search_query=push+up+proper+form",
+"Mountain climbers": "https://www.youtube.com/results?search_query=mountain+climbers+exercise",
+"Biceps curls": "https://www.youtube.com/results?search_query=bicep+curls+proper+form",
+"Triceps pushdown": "https://www.youtube.com/results?search_query=tricep+pushdown+form",
+"Bench press": "https://www.youtube.com/results?search_query=bench+press+proper+form",
+"Burpees": "https://www.youtube.com/results?search_query=burpees+exercise+proper+form",
+"Hanging knee raises": "https://www.youtube.com/results?search_query=hanging+knee+raise+abs+exercise"
+
+};
